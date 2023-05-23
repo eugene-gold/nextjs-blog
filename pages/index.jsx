91489c47c9da493828098c8ec4c3a3a58ee97633
eugene_asus <uran2@bk.ru>
2023-05-24 07:14:01 +0800

    myblog nextjs init

---
+++ pages/index.jsx
@@ -0,0 +1,51 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from "next/link";
+import Layout, {siteTitle} from "../components/layout";
+import utilStyles from '../styles/utils.module.css'
+import {getSortedPostsData} from "../lib/posts";
+import Date from "../components/date";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+    return {
+      props: {
+          allPostsData
+      }
+    }
+}
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+
+      <section className={utilStyles.headingMd}>
+        <p className={styles.description}>
+            Привет! меня зовут Евгений, я разработчик (React)  и переводчик (Русский/Китайский).
+
+        </p>
+          <p className={styles.description}> Мне можно написать <a href="mailto:goldin.irk@gmail.com">на почту</a></p>
+        <p className={styles.description}>Информацию по разработке можно посмотреть <Link href="posts/post">здесь</Link></p>
+      </section>
+
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+            <h2 className={utilStyles.headingLg}>Blog</h2>
+            <ul className={utilStyles.list}>
+                {allPostsData.map(({ id, date, title }) => (
+                    <li className={utilStyles.listItem} key={id}>
+                        <Link href={`/posts/${id}`}>{title}</Link>
+                        <br />
+                        <small className={utilStyles.lightText}>
+                            <Date dateString={date}/>
+                        </small>
+                    </li>
+                ))}
+            </ul>
+        </section>
+
+    </Layout>
+  )
+}
---
+++ pages/posts/post.jsx
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import styles from "../../styles/Home.module.css";
+
+export default function FirstPost() {
+
+    return (
+       <Layout>
+           <Head>
+               <title>О разработке</title>
+           </Head>
+           <>
+               <div className={styles.grid}>
+                   <a href="https://github.com/eugene-gold" className={styles.card}>
+                       <h3>GitHub &rarr;</h3>
+                       <p>Больше информации по разработке и репозиториям</p>
+                   </a>
+
+                   <a href="https://warm-crisp-5889a8.netlify.app" className={styles.card}>
+                       <h3>Магазин одежды &rarr;</h3>
+                       <p>Магазинчик с корзиной и оплатой, Redux, React</p>
+                   </a>
+
+                   <a
+                       href="https://famous-meringue-a923fb.netlify.app/"
+                       className={styles.card}
+                   >
+                       <h3>Образец приложения &rarr;</h3>
+                       <p>Приложение, работающее с данными Stars-Wars Api,
+                           и Zustand (не закончено).
+                       </p>
+                   </a>
+
+                   <a
+                       href="https://singular-tulumba-7f4dfd.netlify.app/"
+                       className={styles.card}
+                   >
+                       <h3>Простой поиск &rarr;</h3>
+                       <p>
+                           Страница простого поиска по данным на React.
+                       </p>
+                   </a>
+               </div>
+           </>
+       </Layout>
+           )
+}
